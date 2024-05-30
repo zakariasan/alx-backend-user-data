3ed8e07ad9dad73228746aab180d6ac718f45ed0
zakariasan <elitziko@gmail.com>
2024-05-30 14:17:52 +0100

    filter OII

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ import re
 from typing import List
 import logging
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 def filter_datum(
         fields: List[str],
@@ -33,3 +35,15 @@ class RedactingFormatter(logging.Formatter):
         """ format """
         msg = super().format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """ get logger """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    str_hand = logging.StreamHandler()
+    str_hand.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(str_hand)
+    return logger
