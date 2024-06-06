9176e52b2682ec73d51e8e90c06f324eb91baa41
zakariasan <elitziko@gmail.com>
2024-06-06 02:30:29 +0100

    error unauthorized 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,15 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def authorized():
+    """GET /api/v1/unauthorized
+    Return:
+        - raise a 401 error
+    """
+    abort(401)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
