58bfef77ffc075fddde1055beb5740cdccad418f
zakariasan <elitziko@gmail.com>
2024-06-06 14:43:20 +0100

    auth init fix:

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,8 @@ from flask import request
 from typing import List, TypeVar
 
 
-class Auth():
+class Auth:
+    """ class to manage API """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Determines if auth is requred for path """
         return False
