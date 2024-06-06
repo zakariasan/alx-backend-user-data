4455457d8b48a55253c9013a081c8d9cd0a3163e
zakariasan <elitziko@gmail.com>
2024-06-06 02:08:23 +0100

    server start up

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -5,9 +5,9 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
 import os
 
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
