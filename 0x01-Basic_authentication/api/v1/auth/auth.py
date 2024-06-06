dea473aa369901b9f8351c556d1f836a59c725a7
zakariasan <elitziko@gmail.com>
2024-06-06 14:37:17 +0100

    auth init files

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if auth is requred for path """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ auth header from req"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return the authorization_header """
+        return None
