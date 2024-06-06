10d674b3ce36c3dca4eb8db44c788573a7f90dc0
zakariasan <elitziko@gmail.com>
2024-06-06 14:59:30 +0100

    update: require_auth func

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,18 @@ from typing import List, TypeVar
 
 class Auth:
     """ class to manage API """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Determines if auth is requred for path """
-        return False
+        if (path is None or excluded_paths is None or not excluded_paths):
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        excluded_paths = [item if item.endswith(
+            '/') else item + '/' for item in excluded_paths]
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ auth header from req"""
