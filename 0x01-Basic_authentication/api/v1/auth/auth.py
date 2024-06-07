906f5647fedc4a6d3f6cb30f2366f01808f59151
zakariasan <elitziko@gmail.com>
2024-06-08 00:25:28 +0100

    excuded path fnmatch

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ Route module for the API
 
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
 class Auth:
@@ -12,19 +13,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Determines if auth is requred for path """
-        if (path is None or excluded_paths is None or excluded_paths == []):
+        if path is None or not excluded_paths:
             return True
-        if path[-1] != '/':
-            path += '/'
-        for excluded_path in excluded_paths:
-            if excluded_path[-1] != '/':
-                excluded_path += '/'
 
-            if excluded_path.endswith('*'):
-                if path.startswith(excluded_path[:-1]):
-                    return False
-            elif path == excluded_path:
+        if path[-1] == '/' and path != '/':
+            path = path[:-1]
+
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '/' and excluded_path != '/':
+                excluded_path = excluded_path[:-1]
+
+            if fnmatch.fnmatch(path, excluded_path):
                 return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
